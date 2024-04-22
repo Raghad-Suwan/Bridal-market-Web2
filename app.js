3aa21afc334b8be526ae85e3186887cd2c7869e1
EmanSOthman <117042251+EmanSOthman@users.noreply.github.com>
2024-04-22 20:23:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const app = express();
+const port = 5000; // أو أي منفذ آخر يمكنك تحديده
+
+// تعيين محرك عرض EJS
+app.set('view engine', 'ejs');
+
+// تحديد مجلد الملفات الثابتة (CSS، الصور، الوسائط)
+app.use(express.static('public'));
+
+// تحديد مسارات الصفحات والمعالجة
+app.get('/', (req, res) => {
+  res.render('home'); // تقديم صفحة الصفحة الرئيسية
+});
+
+// إضافة المزيد من مسارات الصفحات هنا إذا لزم الأمر
+
+// تشغيل التطبيق على المنفذ المحدد
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
