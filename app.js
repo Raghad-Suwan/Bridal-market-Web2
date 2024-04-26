ef6c00d21fe59f89c3793e098c49ba25bd3fc596
danaabdulhaq <danarabdulhaq@gmail.com>
2024-04-26 15:22:26 +0300

    testdashbord

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-
 const express = require('express');
 const app = express();
-const port = 5000; // أو أي منفذ آخر يمكنك تحديده
+const port = 5000; 
+const dashbordRoutes = require('./routes/dashbord/dashbord_routes')
+const path  = require('path');
 
-// تعيين محرك عرض EJS
 app.set('view engine', 'ejs');
 
-// تحديد مجلد الملفات الثابتة (CSS، الصور، الوسائط)
-app.use(express.static('public'));
+const publicDir = path.join(__dirname ,'./public');
+app.use(express.static(publicDir));
+app.use('/dashbord', dashbordRoutes );
 
-// تحديد مسارات الصفحات والمعالجة
-app.get('/', (req, res) => {
-  res.render('home'); // تقديم صفحة الصفحة الرئيسية
-});
-
-// إضافة المزيد من مسارات الصفحات هنا إذا لزم الأمر
-
-// تشغيل التطبيق على المنفذ المحدد
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
