43b1f5d61c716e77bded2a84370ff767391d33bf
hadi sawalmeh <130775763+12114975@users.noreply.github.com>
2024-04-26 22:59:34 +0300

    update product pages

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
+const express = require('express'); 
+const app = express(); 
+const port = 5000; // أو أي منفذ آخر يمكنك تحديده 
+ 
+// تعيين محرك عرض EJS 
+app.set('view engine', 'ejs'); 
+ 
+// تحديد مجلد الملفات الثابتة (CSS، الصور، الوسائط) 
+app.use(express.static('public')); 
+ 
+// تحديد مسارات الصفحات والمعالجة 
+app.get('/', (req, res) => { 
+  res.render('home'); // تقديم صفحة الصفحة الرئيسية 
+}); 
+ 
+const events =require('./routes/event-routes')
+app.use("/events", events)
 
-const express = require('express');
-const app = express();
-const port = 5000; // أو أي منفذ آخر يمكنك تحديده
 
-// تعيين محرك عرض EJS
-app.set('view engine', 'ejs');
-
-// تحديد مجلد الملفات الثابتة (CSS، الصور، الوسائط)
-app.use(express.static('public'));
-
-// تحديد مسارات الصفحات والمعالجة
-app.get('/', (req, res) => {
-  res.render('home'); // تقديم صفحة الصفحة الرئيسية
-});
-
-// إضافة المزيد من مسارات الصفحات هنا إذا لزم الأمر
-
-// تشغيل التطبيق على المنفذ المحدد
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// إضافة المزيد من مسارات الصفحات هنا إذا لزم الأمر 
+ 
+// تشغيل التطبيق على المنفذ المحدد 
+app.listen(port, () => { 
+  console.log(`Example app listening at http://localhost:${port}`); 
 });
