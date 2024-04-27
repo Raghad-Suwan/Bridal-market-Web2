ca2e3ed2aba022a520eaaab6361a0c0e70a966f5
danaabdulhaq <danarabdulhaq@gmail.com>
2024-04-27 23:28:28 +0300

    route version 1.0
    dashbord pages, profile pages ,home page , products pages

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 
 const express = require('express');
 const app = express();
-const path=require('path');
 const port = 5000; 
-
+const dashbordRoutes = require('./routes/dashbord/dashbord_routes')
+const path  = require('path');
 
 app.set('view engine', 'ejs');
 
 
-app.use(express.static(path.join(__dirname,'public')));
- app.use(express.urlencoded({extended:false}));
+
+const publicDir = path.join(__dirname ,'./public');
+
+const events =require('./routes/event-routes')
+const profiles =require("./routes/profiles/profiles_routes");
+const home = require('./routes/home/home-routes')
+const loading = require('./routes/sign/loadingpage')
+const productpage = require('./routes/productPage/product-route')
 
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.use(express.static(publicDir));
+
+app.use('/dashbord', dashbordRoutes );
+app.use("/events", events);
+app.use('/profiles', profiles );
+app.use('/loading',loading);
+app.use('/productpage',productpage);
+
+
+
+// app.get('/home', (req, res) => {
+//   res.render('home');
+// });
+
+app.use('/home', home); 
 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
---
+++ routes/home/home-routes.js
@@ -0,0 +1,14 @@
+const { render } = require('ejs');
+const express = require('express');
+const router = express.Router();
+
+
+router.get("/home", (req,res)=>{
+    res.render("home")
+    })
+    
+
+
+
+
+    module.exports = router; 
