a37446d435945b440877a43d218bc522f03e1127
RawanIssa <s12217586@stu.najah.edu>
2024-05-05 17:55:44 +0300

    Description of the changes made

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const path  = require('path');
 app.set('view engine', 'ejs');
 
 
-
-
-
 const publicDir = path.join(__dirname ,'./public');
 const dashbordRoutes = require('./routes/dashbord/dashbord_routes')
 const events =require('./routes/event/event-routes')
@@ -32,6 +29,30 @@ app.use('/productpage',productpage);
 app.use('/signup',signupUser);
 app.use('/home', home); 
 app.use('/signupProvider',signupProvider);
+
+
+const product = {
+  title: 'Sample Product',
+  price: 80,
+  description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+  image: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png',
+  images: [
+    'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png',
+    'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png',
+    'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png',
+  ]
+};
+const similarProducts = [
+  { title: 'Similar Product 1', brand: 'Brand A', price: 20, image: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
+  { title: 'Similar Product 2', brand: 'Brand B', price: 30, image: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
+  { title: 'Similar Product 3', brand: 'Brand C', price: 25, image: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
+  { title: 'Similar Product 4', brand: 'Brand D', price: 52, image: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' }
+];
+app.get('/', (req, res) => {
+  res.render('ProductPage', { product, similarProducts });
+});
+
+
 
 
 
