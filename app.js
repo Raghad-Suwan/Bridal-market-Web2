5415d4fd7fcc1d6eeb917ac82a98f1a9d5c51269
hadi sawalmeh <130775763+12114975@users.noreply.github.com>
2024-05-14 04:02:16 +0300

    create data and connect the visual and Editing the pages to be dynamic in the data

--- app.js
+++ app.js
@@ -1,79 +1,79 @@
-
+//express and path 
 const express = require('express');
 const app = express();
-const port = 5000; 
-const path  = require('path');
-
+const port = 5000;
+const path = require('path');
 app.set('view engine', 'ejs');
 
 // mongodb connect
-const mongoose = require("mongoose"); 
-mongoose.connect("mongodb://localhost:27017/crud") 
- 
-const UserSchema= new mongoose.Schema({ 
-  name: String, 
-  age: Number 
-}) 
- 
-const UserModel =mongoose.model("users",UserSchema) 
- 
-app.get("/getUsers",(req, res) =>{ 
- 
-  UserModel.find({}).then(function(users){ 
-    res.json(users) 
-  }).catch(function(err){ 
-    console.log(err) 
-  }) 
-})
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/crud")
+const Users =require('./models/userschema')
 
+
+
+
+
+
+
+//models
 
 
 //routs
-const publicDir = path.join(__dirname ,'./public');
+const publicDir = path.join(__dirname, './public');
 const dashbordRoutes = require('./routes/dashbord/dashbord_routes')
-const events =require('./routes/event/event-routes')
-const profiles =require("./routes/profiles/profiles_routes");
+const events = require('./routes/event/event-routes')
+const profiles = require("./routes/profiles/profiles_routes");
 const home = require('./routes/home/home-routes')
 const loading = require('./routes/sign/loadingpage')
 const productpage = require('./routes/productPage/product-route')
-
+const calender1 = require("./routes/calender/calender")
 const signupUser = require('./routes/sign/sign-routes')
 const signupProvider = require('./routes/sign/sign-routes');
 
 
+
 app.use(express.static(publicDir));
-
-app.use('/dashbord', dashbordRoutes );
+app.use('/calender1', calender1);
+app.use('/calender2', calender1);
+app.use('/dashbord', dashbordRoutes);
 app.use('/eventproduct', events);
-app.use('/profiles', profiles );
-app.use('/loading',loading);
-app.use('/productpage',productpage);
-app.use('/signup',signupUser);
-app.use('/', home); 
-app.use('/signupProvider',signupProvider);
+app.use('/profiles', profiles);
+app.use('/loading', loading);
+app.use('/productpage', productpage);
+app.use('/signup', signupUser);
+app.use('/', home);
+app.use('/signupProvider', signupProvider);
 
- 
 
-//dynamic product 
-app.get('/eventproduct', (req, res) => {
-  const products = [
-      { id: 1, title: 'Piece Jigsaw Puzzle', brand: 'Ravensburger', price: 19.99, category:"dress" ,src: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png'} ,
-      { id: 2, title: 'Etch A Sketch', brand: 'Ohio Art', price: 21.99, category:"camera",src: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
-      { id: 3, title: 'Piece Jigsaw Puzzle', brand: 'Raveasdnsburger', price: 19.99,category:"cake" ,src: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
-      { id: 4, title: 'Piece Jigsaw Puzzle', brand: 'Ravensburger', price: 19.99, category:"plapla" , src: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
-      { id: 5, title: 'Piece Jigsaw Puzzle', brand: 'Ravensburger', price: 19.99, category:"plapla" , src: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
-      { id: 6, title: 'Piece Jigsaw Puzzle', brand: 'Ravensburger', price: 19.99, category:"plapla" , src: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
-      { id: 7, title: 'Piece Jigsaw Puzzle', brand: 'Ravensburger', price: 19.99, category:"plapla" , src: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
-      { id: 8, title: 'Piece Jigsaw Puzzle', brand: 'Ravensburger', price: 19.99, category:"plapla" , src: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
-      { id: 9, title: 'Piece Jigsaw Puzzle', brand: 'Ravensburger', price: 19.99, category:"plapla" , src: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' },
-  ];
-res.render('index', { products: products });
-app.get('/productpage/:categoryName', (req, res) => {
-  const categoryName = req.params.categoryName;
-  const filteredProducts = products.filter(p => p.category === categoryName);
-  res.render('/productpage/productpage', { products: filteredProducts });
+
+
+
+app.get("/eventproduct", (req, res) => {
+    Users.find()
+    .then((result) => {res.render("index",{products: result})})
+    .catch((err) => {
+      console.log(err);
+    } )
+  })
+
+
+  //dynamic product 
+
+  app.get('/productpage/:categoryName', async (req, res) => {
+    const categoryName = req.params.categoryName;
+
+    try {
+        // استرجاع المنتجات المتعلقة بالفئة categoryName من قاعدة البيانات
+        const filteredProducts = await Users.find({ category:categoryName });
+
+        res.render('index', { products: filteredProducts });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('خطأ في استرجاع البيانات');
+    }
 });
-});
+
 
 //To go to the desired image
 
@@ -84,5 +84,3 @@ app.get('/productpage/:categoryName', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
---
+++ models/userschema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//define the schema
+const productscema = new Schema({
+id:Number,
+brand:String,
+price:Number,
+title:String,
+src:String,
+category:String,
+},{
+    min: 9,
+    max: 9
+})
+
+
+//create a model based in the schema
+const Provider = mongoose.model('users', productscema)
+
+
+
+//export the model  
+module.exports = Provider
